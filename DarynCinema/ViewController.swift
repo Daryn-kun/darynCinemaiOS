c6af1abcae0bf71d53f824e145066e24c5bad7af
Daryn Nurlan <daryn.123434@gmail.com>
2020-10-16 00:38:20 -0700

    Initial Commit

---
+++ DarynCinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarynCinema
+//
+//  Created by nagato on 10/16/20.
+//  Copyright © 2020 nagato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
